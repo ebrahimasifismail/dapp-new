e8c6b8f722b9df98a4b588149de655088a89895d
ebrahimasifismail <ebrahimasifismail@gmail.com>
2019-05-29 18:37:24 +0530

    initial commit

---
+++ contracts/Store.sol
@@ -0,0 +1,36 @@
+pragma solidity ^0.5.0;
+
+contract Store {
+    struct Product {
+        uint id;
+        string name;
+        uint price;
+        uint stock;
+    }
+
+    mapping(uint => Product) public products;
+
+    mapping(address => Product) public buyers;
+
+    uint public productCount;
+
+    constructor () public {
+        addProduct("Product 1", 10);
+        addProduct("Product 2", 20);
+    }
+
+    function addProduct (string memory _name, uint _stock) private {
+        productCount ++;
+        products[productCount] = Product(productCount, _name, 100, _stock);
+
+    }
+    function buy (uint _productId) public payable {
+
+        require (products[_productId].stock > 0, "");
+
+        buyers[msg.sender] = products[_productId];
+        products[_productId].stock --;
+
+
+    }
+}
---
+++ src/js/app.js
@@ -0,0 +1,123 @@
+App = {
+  web3Provider: null,
+  contracts: {},
+  account: '0x0',
+
+  init: async function() {
+    // Load pets.
+    // $.getJSON('../pets.json', function(data) {
+    //   var petsRow = $('#petsRow');
+    //   var petTemplate = $('#petTemplate');
+
+    //   for (i = 0; i < data.length; i ++) {
+    //     petTemplate.find('.panel-title').text(data[i].name);
+    //     petTemplate.find('img').attr('src', data[i].picture);
+    //     petTemplate.find('.pet-breed').text(data[i].breed);
+    //     petTemplate.find('.pet-age').text(data[i].age);
+    //     petTemplate.find('.pet-location').text(data[i].location);
+    //     petTemplate.find('.btn-adopt').attr('data-id', data[i].id);
+
+    //     petsRow.append(petTemplate.html());
+    //   }
+    // });
+
+    return App.initWeb3();
+  },
+
+  initWeb3: async function() {
+    if (window.ethereum) {
+      App.web3Provider = window.ethereum;
+      try {
+        await window.ethereum.enable();
+      }catch (error) {
+        console.error("User denied account access")
+      }
+    }
+    else if (window.web3) {
+      App.web3Provider = window.web3.currentProvider;
+
+    }
+    else {
+      App.web3Provider = new Web3.provider.HttpProvider('http://localhost:7545');
+      web3 = new web3(App.web3Provider);
+    }
+    
+    return App.initContract();
+  },
+
+  initContract: function() {
+    $.getJSON("Store.json", function(store){
+      App.contracts.Store = TruffleContract(store);
+
+      App.contracts.Store.setProvider(App.web3Provider);
+      return App.render();
+    })
+  },
+  render: function() {
+    var StoreInstance;
+    var loader = $("#loader");
+    var content = $("#content");
+
+
+    loader.show();
+    content.hide();
+
+    web3.eth.getCoinbase(function (err, account){
+      if(err === null) {
+        App.account = account;
+        $("#accountAddress").html("Your Account: " + account);
+      }
+    });
+
+    App.contracts.Store.deployed().then(function(instance) {
+      StoreInstance = instance;
+      return StoreInstance.productCount();
+
+    }).then(function(productCount){
+      var productResults = $("#candidatesResults");
+      productResults.empty();
+
+      var productSelect = $('#candidatesSelect');
+      productSelect.empty();
+
+      for (var i = 1; i<= productCount; i++) {
+        StoreInstance.products(i).then(function(product){
+          var id = product[0];
+          var name = product[1];
+          var price = product[2];
+          var stock = product[3];
+
+          var productTemplate = "<tr><th>" + id + "</th><td>" + name + "</td><td>" + price + "</td><td>"+ stock +"</td></tr>"
+          productResults.append(productTemplate);
+
+          var productOption = "<option value='" + id + "' >" + name + "</ option>"
+          productSelect.append(productOption);
+        });
+      }
+      
+      loader.hide()
+      content.show()
+
+    }).catch(function(error) {
+      console.warn(error);
+    });
+  },
+  castVote: function() {
+    var productId = $('#candidatesSelect').val();
+    App.contracts.Store.deployed().then(function(instance){
+      return instance.buy(productId, {from: App.account})
+    }).then(function(result){
+      $("#content").hide();
+      $("#loader").show();
+    }).catch(function(err){
+      console.error(err);
+    });
+  }
+
+};
+
+$(function() {
+  $(window).load(function() {
+    App.init();
+  });
+});
